6cc6de9d6a74444573f45904afc928244469a26f
craftycheshire <mini_vann@ymail.com>
2024-08-15 20:38:21 -0500

    day 4 (5) done

--- day-5.js
+++ day-5.js
@@ -22,6 +22,9 @@ const songs = [];
 
 // Example: 
 songs.push(createSong('Yesterday', 'The Beatles', 1965));
+songs.push(createSong('What a Wonderful World', 'Louis Armstrong', 1967));
+songs.push(createSong('Over the Rainbow', 'Judy Garland', 1967));
+songs.push(createSong('No Rain', 'Blind Melon', 1993));
 
 console.table(songs);
 
@@ -36,6 +39,9 @@ console.log("\nExample 2: Accessing Objects in the Array");
 // Example
 console.log("First song: Title = " + songs[0].title + ", Artist = " + songs[0].artist);
 // TODO: Access and log the details of the rest of the songs in the array
+console.log("Second song: Title = " + songs[1].title + ", Artist = " + songs[1].artist);
+console.log("Third song: Title = " + songs[2].title + ", Artist = " + songs[2].artist);
+console.log("Fourth song: Title = " + songs[3].title + ", Artist = " + songs[3].artist);
 
 console.log("\nExample 3: Adding More Songs");
 
@@ -47,6 +53,8 @@ console.log("\nExample 3: Adding More Songs");
 songs.push(createSong('My Way', 'Frank Sinatra', 1969));
 songs.push(createSong('Imagine', 'John Lennon', 1971));
 // TODO 3: Add two songs more of your own
+songs.push(createSong('Barbie Girl', 'Aqua', 1997));
+songs.push(createSong('Hanging by a Moment', 'Lifehouse', 2000));
 
 console.table(songs);
 
@@ -59,18 +67,20 @@ console.log("\nExample 4: Accessing New Songs");
 songs[3].title = "Puff the Magic Dragon";
 songs[3].artist = "Peter, Paul, and Mary";
 
-console.log("Fourth song: Title = " + songs[3].title + ", Artist = " + songs[3].artist);
+console.log("Fourth song changed to: Title = " + songs[3].title + ", Artist = " + songs[3].artist);
 
 // TODO 4:
 // Modify the details of the fifth song
 // - Artist and year of the fifth song
+songs[4].artist = "Elvis Presley";
+songs[4].year = 1973;
 
 
 
 console.log("\nExample 5: Modifying the Array");
 
 // Example 5: Modifying the Array
-// Complete replace the details of the first song with the following:
+// Completely replace the details of the first song with the following:
 // - Title: 'Eleanor Rigby'
 // - Artist: 'The Beatles'
 // - Year: 1966
@@ -81,3 +91,7 @@ console.log("Updated first song: Title = " + songs[0].title + ", Artist = " + so
 
 // TODO 5: Completely Replace the details of the second song.
 
+songs[1] = createSong('Brains', 'Voltaire', 2002);
+console.log("Updated second song: Title = " + songs[1].title + ", Artist = " + songs[1].artist + ", Year = " + songs[1].year);
+
+console.table(songs);
